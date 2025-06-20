a0f8aa32c742fa3a5f8f5e5baf48760b73442bc5
Jonathan <alayj3665@gmail.com>
2025-06-20 09:05:43 -0500

    Creaciacion de predio.php

--- app/Models/Predio.php
+++ app/Models/Predio.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Predio extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'listado_predios';
+
+    protected $fillable = [
+        'propietario', 'clave',
+        'latitud1', 'longitud1',
+        'latitud2', 'longitud2',
+        'latitud3', 'longitud3',
+        'latitud4', 'longitud4',
+        'hora_p1', 'hora_p2', 'hora_p3'
+    ];
 }
+
